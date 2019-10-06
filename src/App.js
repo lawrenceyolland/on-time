51d598082fc827166d7d046c6b9e3af51e4492e1
github username <lawrence@sciencedisrupt.com>
2019-10-06 14:21:44 +0100

    api setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
+import NoticeBoard from "./containers/NoticeBoard";
 
 class App extends Component {
+  componentDidMount() {
+    console.log("🎫", process.env.REACT_APP_ID);
+    console.log("🔑", process.env.REACT_APP_KEYS);
+  }
+
   render() {
-    console.log("here's the id 🎫", process.env.REACT_APP_ID);
-    console.log("here's the key 🔑", process.env.REACT_APP_KEYS);
     return (
       <div className="App">
         <header className="App-header">
           <h1>is the train running?</h1>
         </header>
+        <NoticeBoard />
       </div>
     );
   }
