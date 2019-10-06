e927a6d2e38123625496f173e53d51cae7c1420b
github username <lawrence@sciencedisrupt.com>
2019-10-06 13:53:02 +0100

    set up environment variables for app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log("here's the id 🎫", process.env.REACT_APP_ID);
+    console.log("here's the key 🔑", process.env.REACT_APP_KEYS);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>is the train running?</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
